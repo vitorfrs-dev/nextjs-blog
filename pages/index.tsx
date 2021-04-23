bf191cae1b9ea44c5beb647ee76e21884b44f19e
Vitor Ferreira <vitorfrs98@gmail.com>
2021-04-22 22:34:33 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <h1>Next JS Blog</h1>
+      </div>
+    </>
+  )
+}
